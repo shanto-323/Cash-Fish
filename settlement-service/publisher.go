eed0c09bb1e6c5f8be800a5068996ec37168253d
SHANTO323 <shantochando323@gmail.com>
2025-05-31 02:52:54 +0600

    publisher

---
+++ settlement-service/publisher.go
@@ -0,0 +1,100 @@
+package settlementservice
+
+import (
+	"context"
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Publisher struct {
+	conn *amqp.Connection
+	ch   *amqp.Channel
+}
+
+func NewPublisher(rabbitmqUrl string) (*Publisher, error) {
+	conn, err := amqp.Dial(rabbitmqUrl)
+	if err != nil {
+		return nil, err
+	}
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Publisher{
+		conn: conn,
+		ch:   ch,
+	}, nil
+}
+
+func (p *Publisher) CloseConn() error {
+	return p.conn.Close()
+}
+
+func (p *Publisher) CloseChan() error {
+	return p.conn.Close()
+}
+
+func (p *Publisher) Consumer(ctx context.Context, sourceQueue string, targetQueue string) error {
+	_, err := p.ch.QueueDeclare(
+		sourceQueue,
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+
+	msgs, err := p.ch.Consume(
+		sourceQueue,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		for msg := range msgs {
+			if err := p.Produce(ctx, targetQueue, msg); err != nil {
+				log.Print(err)
+				return
+			}
+		}
+	}()
+	return nil
+}
+
+func (p *Publisher) Produce(ctx context.Context, targetQueue string, msg amqp.Delivery) error {
+	_, err := p.ch.QueueDeclare(
+		targetQueue,
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+
+	return p.ch.PublishWithContext(
+		ctx,
+		"",
+		targetQueue,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        msg.Body,
+		},
+	)
+}
